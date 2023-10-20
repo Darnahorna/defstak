93d2898e20991c6a202f0d0d84765eb36b9d2fcf
darnahorna <darnahorna@gmail.com>
2023-10-20 15:25:34 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Navigation from './components/Navigation.vue'
+import SideBar from './components/SideBar.vue'
+import ManageUsersPage from './components/pages/ManageUsersPage.vue'
+</script>
+
+<template>
+  <header class="header">
+    <Navigation></Navigation>
+  </header>
+  <main class="main"><ManageUsersPage></ManageUsersPage></main>
+  <div class="sidebar"><SideBar></SideBar></div>
+  <footer class="footer">Footer</footer>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <!-- <li><div>Dashboard</div></li> -->
+    <li class="nav-link">
+      <UsersEdit></UsersEdit>
+      Manage Users
+    </li>
+    <!-- <li><div>Parameters</div></li> -->
+  </ul>
+</template>
+
+<script setup>
+import UsersEdit from './icons/UsersEdit.vue'
+</script>
+
+<style scoped>
+.nav-link {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem 1rem;
+  height: 3.5rem;
+  gap: 0.5rem;
+}
+.nav-link:hover {
+  color: var(--primary-color);
+}
+.image {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+</style>
